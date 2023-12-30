e7a27fe7380ea5c41b6e38b0b16176b40c1742e9
Ramya <ramya.bala221190@gmail.com>
2023-12-30 14:34:00 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'elements-app-2';
+  @ViewChild('testElement')testElement:ElementRef | undefined;
+
+  constructor(private renderer:Renderer2){}
+
+  ngAfterViewInit(){
+    this.renderer.listen(this.testElement?.nativeElement,'computed',(event)=>{
+      console.log(event)
+    })
+  }
 }
